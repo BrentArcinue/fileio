957e25f67a1c63eea346bbe645101ff5722b921c
Brent Arcinue <brentarcinuegithub@gmail.com>
2019-10-23 14:31:42 -0700

    Initial Commit

---
+++ FileIO/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FileIO
+//
+//  Created by Arcinue, Brent Gregory on 10/23/19.
+//  Copyright © 2019 Euler's Constant. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
