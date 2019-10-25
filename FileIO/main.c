2fb8c4c1a7cb9d2145dd6db87829d5b6499b25ca
Brent Arcinue <brentarcinuegithub@gmail.com>
2019-10-25 15:19:21 -0700

    10/25

--- FileIO/main.c
+++ FileIO/main.c
@@ -10,11 +10,27 @@
 
 int main(void){
     
-    char function[20];
-    
-    printf("What function would you like to find?");
-    scanf("%s", function);
-    
-    
-    return 0;
-}
+    char file[100];
+    printf("What file do you want to open?");
+    scanf("%s", file);
+        /* Pointer to the file */
+        FILE *fp1;
+        /* Character variable to read the content of file */
+        char c;
+        
+        /* Opening a file in r mode*/
+        fp1= fopen (file, "r");
+        
+        /* Infinite loop –I have used break to come out of the loop*/
+        while(1)
+        {
+            c = fgetc(fp1);
+            if(c==EOF)
+                break;
+            else
+                printf("%c", c);
+        }
+        fclose(fp1);
+        return 0;
+    }
+
---
+++ FileIO/test.c
@@ -0,0 +1,12 @@
+//
+//  test.c
+//  FileIO
+//
+//  Created by Arcinue, Brent Gregory on 10/25/19.
+//  Copyright © 2019 Euler's Constant. All rights reserved.
+//
+
+#include <stdio.h>
+
+int test(void);
+
